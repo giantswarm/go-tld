c27cd56472ad32b77d4214b24583a5dd66dc5ee0
inhies <inhies@gmail.com>
2013-10-28 19:17:30 -1000

    Fix comments

--- tld.go
+++ tld.go
@@ -1,8 +1,7 @@
 // Package tld provides a way to verify that a given top level domain name is
 // valid. It comes with a predefined list however it can be updated at runtime
 // by running tld.Update(url) where url points to a text file containing a list
-// of acceptable TLDs./ Package tld provides a means for validating top level
-// domain names.
+// of acceptable TLDs.
 package tld
 
 import (
